7b0261ea741fbc0722e694d47b0649b9ee51681a
vitoria <finkedificacoes@gmail.com>
2023-11-04 18:17:39 +0000

    tentativa de report com mocha

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,26 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'mochawesome-report',
+    overwrite: false,
+    html: true,
+    json: true,
+  },
+  e2e: {
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+    },
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-  
+
   e2e: {
-      baseUrl:"http://localhost:3000/",
-   }
+    baseUrl: "http://localhost:3000/",
+  }
 });
